571105138c02ad2a9a09e179ab80f18359910453
FuwaFuwa0204 <soyeonmail24@gmail.com>
2023-05-03 18:20:26 +0900

    add folderable icon, spring animation, animateColorAsState

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -21,12 +21,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -35,8 +40,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults.contentWindowInsets
@@ -44,6 +54,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -55,6 +70,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.woof.data.Dog
 import com.example.woof.data.dogs
+import com.example.woof.ui.theme.Green100
 import com.example.woof.ui.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
@@ -86,7 +102,9 @@ fun WoofApp() {
         topBar = { WoofTopAppBar()}
 
     ){
-        LazyColumn(modifier=Modifier.padding(it).background(MaterialTheme.colorScheme.background)){
+        LazyColumn(modifier= Modifier
+            .padding(it)
+            .background(MaterialTheme.colorScheme.background)){
             items(dogs) {
                 DogItem(dog = it)
             }
@@ -128,19 +146,40 @@ fun DogItem(
     modifier: Modifier = Modifier
 ) {
 
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(targetValue = if(expanded) Green100 else MaterialTheme.colorScheme.surface)
+
     Card(modifier=Modifier.padding(8.dp),
          colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surface),
          elevation = CardDefaults.cardElevation(4.dp),
          ){
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(id = R.dimen.padding_small))
-    ) {
-        DogIcon(dog.imageResourceId)
-        DogInformation(dog.name, dog.age)
+        Column(modifier = Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+            )
 
-    }
+        )
+            .background(color)
+        ) {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(Modifier.weight(1f))
+                //버튼을 누르면 expanded의 상태값을 true로 변경.(onClick)
+                DogItemButton(expanded = expanded, onClick = { expanded = !expanded})
+
+            }
+            if(expanded){
+                DogHobby(dogHobby = dog.hobbies)
+            }
+
+        }
     }
 
 }
@@ -171,6 +210,47 @@ fun DogIcon(
 
         contentDescription = null
     )
+}
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick){
+
+        Icon(
+            //expanded가 true면 펼쳐진 아이콘 false면 펼치긴 전 아이콘
+            imageVector = if(expanded)Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colorScheme.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description)
+        )
+
+    }
+
+}
+
+
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.headlineSmall,
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.bodyLarge,
+        )
+    }
 }
 
 /**
